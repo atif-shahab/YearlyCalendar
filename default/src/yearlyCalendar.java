2fd6e473b4214b407945ed79dbfb424c8cd725b7
atif <shahabatif@gmail.com>
2022-09-12 17:36:28 -0700

    generate the days of the year with corresponding epoch time.

--- default/src/yearlyCalendar.java
+++ default/src/yearlyCalendar.java
@@ -1,7 +1,17 @@
 import java.util.ArrayList;
 
+
+// https://www.islamicfinder.us/index.php/api/prayer_times?country=US&zipcode=94582&latitude=37.7767&longitude=-121.9692&method=2&juristic=1&date=1663027200
 class yearlyCalendar {
     public static void main(String[] args) {
+        String endpoint = "https://www.islamicfinder.us/index.php/api/prayer_times";
+        String country = "US";
+        String zipCode = "94582";
+        String latitude= "37.7767";
+        String longitutde = "-121.9692";
+        String method = "2";
+        String juristic = "1";
+
         int year = 2024;
         ArrayList<Day> daysInYear = Days.getDays(year);
 
